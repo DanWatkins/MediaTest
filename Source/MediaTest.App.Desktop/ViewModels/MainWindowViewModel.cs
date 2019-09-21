dff53641b08ff9e73656a485db62555ec03fdbca
Dan Watkins <dwatkins@valpineware.com>
2019-09-21 11:45:07 -0500

    Initial files

---
+++ Source/MediaTest.App.Desktop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaTest.App.Desktop.Windows.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ Source/MediaTest.App.Desktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace MediaTest.App.Desktop.Windows.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
