3a8c51f5cd80bff22077a93b2ed05f156b28617e
Dan Watkins <dwatkins@valpineware.com>
2019-09-21 12:29:53 -0500

    Skia test

--- Source/MediaTest.Client.Desktop/ViewModels/MainWindowViewModel.cs
+++ Source/MediaTest.Client.Desktop/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,36 @@
-﻿namespace MediaTest.Client.Desktop.ViewModels
+﻿using SkiaSharp;
+using System;
+using System.IO;
+
+namespace MediaTest.Client.Desktop.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
         public string Greeting => "Welcome to Avalonia!";
+
+        private SKBitmap _PreviewImage;
+        public SKBitmap PreviewImage
+        {
+            get
+            {
+                if (_PreviewImage == null)
+                {
+                    _PreviewImage = SKBitmap.Decode(@"C:\Users\dwatk\Desktop\test.bmp");
+
+                    using (var resized = new SKBitmap(128, 128))
+                    {
+                        _PreviewImage.ScalePixels(resized, SKFilterQuality.High);
+                        using (var image = SKImage.FromBitmap(resized))
+                        using (var data = image.Encode(SKEncodedImageFormat.Jpeg, 80))
+                        using (var stream = File.OpenWrite($@"C:\Users\dwatk\Desktop\resized-{DateTime.UtcNow.Ticks}.jpg"))
+                        {
+                            data.SaveTo(stream);
+                        }
+                    }
+                }
+
+                return _PreviewImage;
+            }
+        }
     }
 }
